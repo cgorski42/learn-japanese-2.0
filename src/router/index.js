5e04e46b98231ae4fcc0ea32ad84ca196f4d2e8e
nightwingxoracle <cgorski42@gmail.com>
2018-04-18 14:19:33 -0600

    removed unused routes

--- src/router/index.js
+++ src/router/index.js
@@ -3,8 +3,6 @@ import Router from 'vue-router'
 import HomePage from '@/components/HomePage'
 import Options from '@/components/Options'
 import Quiz from '@/components/Quiz'
-import SearchResults from '@/components/SearchResults'
-import HashTag from '@/components/HashTag'
 import UserPage from '@/components/UserPage'
 
 Vue.use(Router)
@@ -27,16 +25,6 @@ export default new Router({
       name: 'Quiz',
       component: Quiz
     },  
-    {
-      path: '/search',
-      name: 'SearchResults',
-      component: SearchResults
-    },
-    {
-      path: '/hashtag/:hashtag',
-      name: 'HashTag',
-      component: HashTag
-    },
     {
       path: '/user/:userID',
       name: 'UserPage',
